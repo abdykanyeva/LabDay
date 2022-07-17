4b72afc61eac1c01146d2df421db7caad55de9c3
adelinaabdykanyeva <adelina.abdykanyeva@gmail.com>
2022-07-16 20:09:25 -0500

    Pizza App

--- src/labSelfPracticePizzaApp/Main.java
+++ src/labSelfPracticePizzaApp/Main.java
@@ -7,35 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Welcome to the Cydeo Pizza Order App");
-
-        ArrayList<User> user = userList(scan);
-        System.out.println(user);
-
-
-    }
-
-    public static ArrayList<User> userList(Scanner scan) {
-
-        ArrayList<User> userList = new ArrayList<>();
-
-        for (int i = 0; i < 3; i++) {
-
-            User user = new User();
-
-            System.out.println("Email: ");
-
-            user.setEmail(scan.next());
-
-            System.out.println("Password");
-            user.setPassword(scan.next());
-
-            userList.add(user);
-
-        }
-        return userList;
     }
 }
 
--- src/labSelfPracticePizzaApp/User.java
+++ src/labSelfPracticePizzaApp/User.java
@@ -1,60 +1,8 @@
 package labSelfPracticePizzaApp;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
 
 public class User {
 
-    private String email;
-    private String password;
 
-    public User(){
-        setEmail(email);
-        setPassword(password);
-    }
-
-    public String getEmail(){
-        return email;
-    }
-
-    public void setEmail(String email){
-        ArrayList<String> domain = new ArrayList<>(Arrays.asList("Gmail", "Hotmail", "Live"));
-        if(email.contains(domain +"")){
-            this.email = email;
-        }else{
-            System.out.println("Email domain can  only be \"Gmail\", \"Hotmail\", \"Live\"");
-            System.exit(1);
-        }
-        int firstIndexOf = email.indexOf('@');
-        int secondIndexOf = email.indexOf(".");
-
-        boolean checkEmail = secondIndexOf < firstIndexOf || secondIndexOf < 0 || firstIndexOf < 0;
-
-        if(checkEmail){
-            this.email = email;
-        }else{
-            System.out.println("Invalid email credentials: " + email);
-            System.exit(1);
-        }
-
-
-
-    }
-
-    public String getPassword(){
-        return password;
-    }
-
-    public void setPassword(String password){
-        this.password = password;
-    }
-
-
-    @Override
-    public String toString() {
-        return getClass().getSimpleName() + "{" +
-                "email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
